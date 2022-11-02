635e9687d8faa420ca3f13f77fc26d7c543688f4
ojarenasg <ojarenasg@unal.edu.co>
2022-11-01 20:05:53 -0500

    prueba

--- pregunta.py
+++ pregunta.py
@@ -12,9 +12,20 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df.dropna(inplace=True)
+    df["sexo"] = df["sexo"].apply(lambda x: str(x).lower().strip())
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x: str(x).lower().strip())
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).lower().strip())
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).lower().strip())
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().strip())
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).replace("-"," ").replace("_"," ").strip())
+    df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().replace("_"," ").replace("-"," "))
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
+    df.drop(['Unnamed: 0'], axis=1,inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+    
     return df
+
